4b867832a683638b3ac65906916d04218597b724
Kavya <kavyamnair482@gmail.com>
2026-01-23 11:31:08 +0530

    final commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\BookingViewController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+////Route::middleware(['auth'])->group(function () {
+//    Route::get('/', [DashboardController::class, 'index']);
+//
+//    Route::get('/bookings', [BookingViewController::class, 'index']);
+//    Route::get('/bookings/create', [BookingViewController::class, 'create']);
+//    Route::post('/bookings', [BookingViewController::class, 'store']);
+//    Route::get('/bookings/{id}/edit', [BookingViewController::class, 'edit']);
+//    Route::put('/bookings/{id}', [BookingViewController::class, 'update']);
+//    Route::delete('/bookings/{id}', [BookingViewController::class, 'destroy']);
+//});
